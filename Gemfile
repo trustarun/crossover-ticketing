d3d6f1de85672f1b9c5433142248eafaad1260c3
Arun <trustarun@yahoo.co.in>
2016-07-09 17:05:16 +0530

    Bootstrap introduced

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ source 'https://rubygems.org'
 gem 'rails', '4.2.5.1'
 gem 'mysql2'
 gem 'sass-rails', '~> 5.0'
+gem 'bootstrap-sass', '~> 3.3.6'
 gem "ember-rails"
 gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.1.0'
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+	def message_type(name)
+	    set_name = name.present? ? name.to_s : ""
+	    case set_name
+	    when "notice"
+	      "success"
+	    when "error"
+	      "error"
+	    when "info"
+	      "info"
+	    when "alert"
+	      "alert"
+	    else
+	      "success"
+	    end
+	 end
 end
